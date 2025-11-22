f36cad2343b97578d44c3c244f23cfe0f59fed7a
mrgee1978@proton.me <mrgee1978@proton.me>
2025-11-22 14:19:00 -0500

    Removed a property that was not used

--- MathGame.TerrenceLGee/MathGame.TerrenceLGee/GameModels/Player.cs
+++ MathGame.TerrenceLGee/MathGame.TerrenceLGee/GameModels/Player.cs
@@ -3,6 +3,5 @@ namespace MathGame.TerrenceLGee.GameModels;
 public class Player
 {
     public string Name { get; set; } = string.Empty;
-    public List<int> Scores { get; set; } = [];
     public List<Game> Games { get; set; } = [];
 }
